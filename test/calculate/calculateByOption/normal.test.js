23a1dedc9b771affff39d8e8d4c0d16f6c1e9343
namunanamu <a11111111@aoyama.jp>
2026-02-15 16:02:49 +0900

    jest: normal のテストの追加および変更
    test\calculate\calculateByOption\normal.test.js
    に対するテストの追加および変更
    
    branch#feature/additional_function

--- test/calculate/calculateByOption/normal.test.js
+++ test/calculate/calculateByOption/normal.test.js
@@ -35,15 +35,14 @@ describe("通常の計算方法のテスト", () => {
         ]);
     });
 
-    test("0人のとき", () => {
+    test("人数が0人のとき", () => {
         let totalAmount = 1000;
         let numberOfPeople = 0;
         
-        const result = normal(totalAmount, numberOfPeople);
-        expect(result).toEqual([]);
+        expect(() => normal(totalAmount, numberOfPeople)).toThrow("通常の計算方法 > message: 人数が1人未満になっています。");
     });
 
-    test("1人のとき", () => {
+    test("人数が1人のとき", () => {
         let totalAmount = 1000;
         let numberOfPeople = 1;
         
@@ -53,6 +52,32 @@ describe("通常の計算方法のテスト", () => {
                 payerCategory: payerCategory.normal,
                 amount: 1000,
                 numberOfPeople: 1
+            }
+        ]);
+    });
+
+    test("金額が0円のとき", () => {
+        let totalAmount = 0;
+        let numberOfPeople = 3;
+        
+        expect(() => normal(totalAmount, numberOfPeople)).toThrow("通常の計算方法 > message: 会計金額が1円未満になっています。");
+    });
+
+    test("金額が1円のとき", () => {
+        let totalAmount = 1;
+        let numberOfPeople = 3;
+        
+        const result = normal(totalAmount, numberOfPeople);
+        expect(result).toEqual([
+            {
+                payerCategory: payerCategory.normal,
+                amount: 1,
+                numberOfPeople: 1
+            },
+            {
+                payerCategory: payerCategory.normal,
+                amount: 0,
+                numberOfPeople: 2
             }
         ]);
     });
