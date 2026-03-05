ba325c4341a767edaf90a602740e63105bcbec64
namunanamu <namunanamu76@gmail.com>
2026-03-06 00:34:05 +0900

    feat: 最低限の画面遷移の挙動の実装
    feature/switching_screen_implementation

--- src/screenSeletor.js
+++ src/screenSeletor.js
@@ -1,9 +1,40 @@
 import titleHTML from './screens/title.html?raw'
+import inputHTML from './screens/input.html?raw'
+import selectOptionHTML from './screens/selectOption.html?raw'
 
 export function screenSelector() {
     const HTMLElement = {
         title: document.getElementById("screen-title"),
+        input: document.getElementById("screen-input"),
+        selectOption: document.getElementById("screen-selectOption")
     };
 
     HTMLElement.title.innerHTML = titleHTML;
+    HTMLElement.input.innerHTML = inputHTML;
+    HTMLElement.selectOption.innerHTML = selectOptionHTML;
+
+    document.addEventListener("click", (e) => {
+        const button = e.target.closest("button");
+        if (!button) return;
+
+        if (button.id === "temp-move-to-screen-input") {
+            changeScreenTo(HTMLElement.input);
+        } else if (button.id === "temp-back-to-screen-title") {
+            changeScreenTo(HTMLElement.title);
+        } else if (button.id === "temp-move-to-screen-selectOption") {
+            changeScreenTo(HTMLElement.selectOption);
+        } else if (button.id === "temp-back-to-screen-input") {
+            changeScreenTo(HTMLElement.input);
+        } else {
+            throw Error("想定されていないボタンIDを持つボタンが押されました。");
+        }
+    });
+}
+
+function changeScreenTo(screenHTMLElement) {
+    document.querySelectorAll(".screen").forEach(element => {
+        element.classList.remove("is-active");
+    });
+
+    screenHTMLElement.classList.add("is-active");
 }
