e35ea94357cab49060ee5cd3019e72382e44eec4
namunanamu <namunanamu76@gmail.com>
2026-03-05 23:21:19 +0900

    refactor: タイトル画面を title.html として外出し
    refactor/prepare_SPA_implementation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { calculate } from "./calculate/calculate.js";
 import { calculateOption } from "./enum/calculateOption.js";
+import { screenSelector } from "./screenSeletor.js";
 
 let totalAmount = 2366;
 let numberOfPeople = 4;
 let extra_info = {};
 console.log(calculate(totalAmount, numberOfPeople, calculateOption.normal, extra_info));
+
+screenSelector();
