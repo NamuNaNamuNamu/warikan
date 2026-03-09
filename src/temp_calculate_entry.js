f1bb3a9b84d36fe42ddc77bbc1bec27a7b1a56b4
namunanamu <namunanamu76@gmail.com>
2026-03-09 18:18:04 +0900

    fix: temp_calculate_entry.js の削除
    refactor/refactor_JS ブランチにて、実際のアプリの挙動にするために、
    もともとエントリポイントに直接書いていた計算ロジックの発動コードを temp_calculate_entry.js に移していた。
    
    コミット: b68d9d75dafcf2e1cb3ab881f71a35ee3a8f0fac
    にて、計算ロジックの仮インプットをテスト化できたので、削除。
    
    branch#fix/delete_temp_calculate_entry

--- src/temp_calculate_entry.js
+++
@@ -1,7 +0,0 @@
-import { calculate } from "./calculate/calculate.js";
-import { calculateOption } from "./enum/calculateOption.js";
-
-let totalAmount = 2366;
-let numberOfPeople = 4;
-let extra_info = {};
-console.log(calculate(totalAmount, numberOfPeople, calculateOption.normal, extra_info));
