b68d9d75dafcf2e1cb3ab881f71a35ee3a8f0fac
namunanamu <namunanamu76@gmail.com>
2026-03-09 18:10:29 +0900

    jest: 飲食量考慮オプションのテストを1つ追加
    いま main.js に書いている動作テストのインプットデータを正式にテストとして採用。
    
    branch#feature/additional_function

--- test/calculate/calculateByOption/considerDifferenceOfPortion.test.js
+++ test/calculate/calculateByOption/considerDifferenceOfPortion.test.js
@@ -1,1 +1,44 @@
-// TODO: 飲食量考慮オプションのテストを書く
+import { considerDifferenceOfPortion } from "../../../src/calculate/calculateByOption/considerDifferenceOfPortion";
+import { payerCategory } from "../../../src/enum/payerCategory";
+
+describe("飲食量考慮オプションのテスト", () => {
+    test("通常パターン1", () => {
+        let totalAmount = 16247;
+        let numberOfPeople = 5;
+
+        let extra_info = {
+            payALot: {
+                amount: 4000,
+                numberOfPeople: 2
+            },
+            payALittle: {
+                amount: 2000,
+                numberOfPeople: 1
+            },
+        }
+        
+        const result = considerDifferenceOfPortion(totalAmount, numberOfPeople, extra_info);
+        expect(result).toEqual([
+            {
+                payerCategory: payerCategory.payALot,
+                amount: 4000,
+                numberOfPeople: 2
+            },
+            {
+                payerCategory: payerCategory.payALittle,
+                amount: 2000,
+                numberOfPeople: 1
+            },
+            {
+                payerCategory: payerCategory.normal,
+                amount: 3124,
+                numberOfPeople: 1
+            },
+            {
+                payerCategory: payerCategory.normal,
+                amount: 3123,
+                numberOfPeople: 1
+            }
+        ]);
+    });
+});
