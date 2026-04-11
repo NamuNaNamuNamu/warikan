052a479703f5534901ff7ebe3b4965182fcd0ccc
namunanamu <namunanamu76@gmail.com>
2026-04-11 23:32:50 +0900

    feat: PayerCategory の拡張
    branch#feature/ui-logic-interface

--- src/enum/PayerCategory.js
+++ src/enum/PayerCategory.js
@@ -16,10 +16,37 @@ export const PayerCategory = Object.freeze({
     }
 });
 
+const PayerCategoryKey = Object.freeze({
+    normal,
+    normalAdjuster,
+    settler,
+    payALittle,
+    payALot
+});
+
+// TODO: キー周りでごちゃごちゃしてきたので整理する。
 export function getPayerCategoryKey (payerCategory) {
-    if (payerCategory === PayerCategory.NORMAL) { return "normal" };
-    if (payerCategory === PayerCategory.NORMAL_ADJUSTER) { return "normalAdjuster" };
-    if (payerCategory === PayerCategory.SETTLER) { return "settler" };
-    if (payerCategory === PayerCategory.PAY_A_LITTLE) { return "payALittle" };
-    if (payerCategory === PayerCategory.PAY_A_LOT) { return "payALot" };
+    if (payerCategory === PayerCategory.NORMAL) { return PayerCategoryKey.normal };
+    if (payerCategory === PayerCategory.NORMAL_ADJUSTER) { return PayerCategoryKey.normalAdjuster };
+    if (payerCategory === PayerCategory.SETTLER) { return PayerCategoryKey.settler };
+    if (payerCategory === PayerCategory.PAY_A_LITTLE) { return PayerCategoryKey.payALittle };
+    if (payerCategory === PayerCategory.PAY_A_LOT) { return PayerCategoryKey.payALot };
+}
+
+export function getPayerCategory (payerCategoryKey) {
+    if (payerCategoryKey === PayerCategoryKey.normal ) { return PayerCategory.NORMAL };
+    if (payerCategoryKey === PayerCategoryKey.normalAdjuster) { return PayerCategory.NORMAL_ADJUSTER };
+    if (payerCategoryKey === PayerCategoryKey.settler) { return PayerCategory.SETTLER };
+    if (payerCategoryKey === PayerCategoryKey.payALittle) { return PayerCategory.PAY_A_LITTLE };
+    if (payerCategoryKey === PayerCategoryKey.payALot) { return PayerCategory.PAY_A_LOT };
+}
+
+export function getPayerCategories (payerCategoryKeys) {
+    const payerCategories = [];
+
+    for (const key of payerCategoryKeys) {
+        payerCategories.push(getPayerCategory(key));
+    }
+
+    return payerCategories;
 }
