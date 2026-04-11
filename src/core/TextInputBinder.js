21f09475decd6f685bdb9d29e3b918b0c81ba0e5
namunanamu <namunanamu76@gmail.com>
2026-04-11 18:12:26 +0900

    refactor: textInputs を共通化
    branch#feature/ui-logic-interface

--- src/core/TextInputBinder.js
+++ src/core/TextInputBinder.js
@@ -1,25 +1,8 @@
-import { inputNumPeople } from "../inputs/inputNumPeople.js";
-import { inputTotalAmount } from "../inputs/inputTotalAmount.js";
-import { inputAmountPayALittle } from "../inputs/inputAmountPayALittle.js";
-import { inputAmountPayALot } from "../inputs/inputAmountPayALot.js";
-import { inputMaximumAppreciationAmount } from "../inputs/inputMaximumAppreciationAmount.js";
-import { inputNumPeoplePayALittle } from "../inputs/inputNumPeoplePayALittle.js";
-import { inputNumPeoplePayALot } from "../inputs/inputNumPeoplePayALot.js";
+import { textInputs } from "../inputs/textInputs.js";
 
 class TextInputBinder {
-    #textInputs = [
-        inputTotalAmount,
-        inputNumPeople,
-
-        inputMaximumAppreciationAmount,
-        inputAmountPayALot,
-        inputNumPeoplePayALot,
-        inputAmountPayALittle,
-        inputNumPeoplePayALittle
-    ];
-
     bindAll () {
-        for (const input of this.#textInputs) {
+        for (const input of textInputs) {
             input.startReceivingInput();
         }
     }
---
+++ src/inputs/textInputs.js
@@ -0,0 +1,18 @@
+import { inputAmountPayALittle } from "./inputAmountPayALittle.js";
+import { inputAmountPayALot } from "./inputAmountPayALot.js";
+import { inputMaximumAppreciationAmount } from "./inputMaximumAppreciationAmount.js";
+import { inputNumPeople } from "./inputNumPeople.js";
+import { inputNumPeoplePayALittle } from "./inputNumPeoplePayALittle.js";
+import { inputNumPeoplePayALot } from "./inputNumPeoplePayALot.js";
+import { inputTotalAmount } from "./inputTotalAmount.js";
+
+export const textInputs = [
+    inputTotalAmount,
+    inputNumPeople,
+
+    inputMaximumAppreciationAmount,
+    inputAmountPayALot,
+    inputNumPeoplePayALot,
+    inputAmountPayALittle,
+    inputNumPeoplePayALittle
+];
