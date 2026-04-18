2ad45eec754ad8c45c965d800169dee7497cfbff
namunanamu <namunanamu76@gmail.com>
2026-04-18 16:04:28 +0900

    feat: 最終結果の文言を修正
    branch#feature/ui-logic-interface

--- src/controller/CalculationResultRenderer.js
+++ src/controller/CalculationResultRenderer.js
@@ -1,27 +1,121 @@
 import { resultFormatter } from "../controller/ResultFormatter.js";
-import { getPayerCategories, getPayerCategoryKey } from "../enum/PayerCategory.js";
-import { WarikanTerm } from "../enum/WarikanTerm.js";
+import { getPayerCategories, getPayerCategory, getPayerCategoryKey, PayerCategory } from "../enum/PayerCategory.js";
 import { warikanResult } from "../screens/selectOption/WarikanResult.js";
 import { animateNumber } from "../utils/animateNumber.js";
+import { hasSameElements } from "../utils/hasSameElements.js";
 
 class CalculationResultRenderer {
     render (result) {
         const formattedResult = resultFormatter.format(result);
+        const outputs = this.#getTextToDisplay(formattedResult);
+        const payerCategoryKeys = Object.keys(outputs);
 
-        const payerCategoryKeys = Object.keys(formattedResult);
-        const payerCategories = getPayerCategories(payerCategoryKeys);
-
-        for (const payerCategory of payerCategories) {
-            const element = warikanResult.getHtmlElement(payerCategory);
-            const numPeopleElement = element.querySelector(`.${WarikanTerm.NUM_PEOPLE.html}`);
-            const amountElement = element.querySelector(`.${WarikanTerm.AMOUNT.html}`);
-            const payerCategoryKey = getPayerCategoryKey(payerCategory);
-            
-            this.#renderHtml({ updateTarget: amountElement, updatedNumber: formattedResult[payerCategoryKey].amount, isAnimated: true });
-            this.#renderHtml({ updateTarget: numPeopleElement, updatedNumber: formattedResult[payerCategoryKey].numberOfPeople });
+        for (const payerCategoryKey of payerCategoryKeys) {
+            for (const key of Object.keys(outputs[payerCategoryKey])) {
+                const payerCategory = getPayerCategory(payerCategoryKey);
+                const element = warikanResult.getHtmlElement(payerCategory);
+                const htmlClass = this.#toHtmlClassName(key);
+                const htmlElement = element.querySelector(`.${htmlClass}`);
+                
+                this.#renderHtml({ updateTarget: htmlElement, updatedNumber: outputs[payerCategoryKey][key] });
+            }
         }
 
+        const payerCategories = getPayerCategories(payerCategoryKeys);
         warikanResult.display(payerCategories);
+    }
+
+    #getTextToDisplay (formattedResult) {
+        const payerCategoryKeys = Object.keys(formattedResult);
+
+        const makeOutput = ({
+            payerCategory = "",
+            numPeople = "",
+            unitNumPeople = "人",
+            ha = "は",
+            amount = "",
+            unitAmount = "円"
+        }) => {
+            return {
+                payerCategory,
+                numPeople,
+                unitNumPeople,
+                ha,
+                amount,
+                unitAmount
+            };
+        };
+
+        // WarikanOption.NORMAL
+        if (hasSameElements(payerCategoryKeys, [
+            getPayerCategoryKey(PayerCategory.NORMAL)
+        ])) {
+            return {
+                [getPayerCategoryKey(PayerCategory.NORMAL)]: makeOutput({
+                    numPeople: formattedResult[getPayerCategoryKey(PayerCategory.NORMAL)].numberOfPeople,
+                    amount: formattedResult[getPayerCategoryKey(PayerCategory.NORMAL)].amount
+                })
+            }
+        }
+
+        if (hasSameElements(payerCategoryKeys, [
+            getPayerCategoryKey(PayerCategory.NORMAL),
+            getPayerCategoryKey(PayerCategory.NORMAL_ADJUSTER)
+        ])) {
+            return {
+                [getPayerCategoryKey(PayerCategory.NORMAL)]: makeOutput({
+                    numPeople: formattedResult[getPayerCategoryKey(PayerCategory.NORMAL)].numberOfPeople,
+                    amount: formattedResult[getPayerCategoryKey(PayerCategory.NORMAL)].amount
+                }),
+                [getPayerCategoryKey(PayerCategory.NORMAL_ADJUSTER)]: makeOutput({
+                    numPeople: formattedResult[getPayerCategoryKey(PayerCategory.NORMAL_ADJUSTER)].numberOfPeople,
+                    amount: formattedResult[getPayerCategoryKey(PayerCategory.NORMAL_ADJUSTER)].amount
+                })
+            }
+        }
+
+        // WarikanOption.CONSIDER_SETTLEMENT
+        if (hasSameElements(payerCategoryKeys, [
+            getPayerCategoryKey(PayerCategory.SETTLER)
+        ])) {
+            return {
+                [getPayerCategoryKey(PayerCategory.SETTLER)]: makeOutput({
+                    payerCategory: PayerCategory.SETTLER.label,
+                    unitNumPeople: "",
+                    amount: formattedResult[getPayerCategoryKey(PayerCategory.SETTLER)].amount
+                })
+            }
+        }
+
+        if (hasSameElements(payerCategoryKeys, [
+            getPayerCategoryKey(PayerCategory.SETTLER),
+            getPayerCategoryKey(PayerCategory.NORMAL)
+        ])) {
+            return {
+                [getPayerCategoryKey(PayerCategory.SETTLER)]: makeOutput({
+                    payerCategory: PayerCategory.SETTLER.label,
+                    unitNumPeople: "",
+                    amount: formattedResult[getPayerCategoryKey(PayerCategory.SETTLER)].amount
+                }),
+                [getPayerCategoryKey(PayerCategory.NORMAL)]: makeOutput({
+                    numPeople: formattedResult[getPayerCategoryKey(PayerCategory.NORMAL)].numberOfPeople,
+                    amount: formattedResult[getPayerCategoryKey(PayerCategory.NORMAL)].amount
+                })
+            }
+        }
+    }
+
+    #toHtmlClassName (key) {
+        const dict = {
+            payerCategory: "payer-category",
+            numPeople: "num-people",
+            unitNumPeople: "unit-num-people",
+            ha: "ha",
+            amount: "amount",
+            unitAmount: "unit-amount"
+        }
+
+        return dict[key];
     }
 
     #renderHtml ({ updateTarget, updatedNumber, isAnimated = false }) {
